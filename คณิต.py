38de90a800a2d6f7d1fefbdfb53c286d463d8f57
GGolfz <wisarutgolf11@gmail.com>
2020-12-05 00:07:22 +0700

    เพิ่ม คณิต

---
+++ คณิต.py
@@ -0,0 +1,20 @@
+import math
+class คณิต:
+    @staticmethod
+    def เพดาน(x):
+        return math.ceil(x)
+    @staticmethod
+    def พื้น(x):
+        return math.floor(x)
+    @staticmethod
+    def แฟกทอเรียล(x):
+        return math.factorial(x)
+    @staticmethod
+    def ค่าสัมบูรณ์(x):
+        return math.fabs(x)
+    @staticmethod
+    def ยกกำลัง(x,y):
+        return math.pow(x,y)
+    @staticmethod
+    def รากที่สอง(x):
+        return math.sqrt(x)
